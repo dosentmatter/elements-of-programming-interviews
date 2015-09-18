752147a29a24c9ceed294aa1e7dd551b8f97c244
Kevin Lau <kelau1993@gmail.com>
2015-09-18 09:12:12 +0100

    modified runtest scripts to run unit/performance tests

--- runtest.bat
+++ runtest.bat
@@ -1,3 +1,22 @@
 @echo off
-python -m unittest discover -v
-pause
+set argC=0
+for %%x in (%*) do set /A argC+=1
+
+if %argC% EQU 0 (
+    python -m unittest discover -v
+) else if  "%~1"=="unit" (
+    python -m unittest discover -v -p test_unit*.py
+) else if "%~1"=="performance" (
+    python -m unittest discover -v -p test_performance*.py
+) else (
+    @echo on
+    echo Incorrect parameter "%~1". Usage: runtest.bat ^[unit^|performance^]
+    @echo off
+)
+
+REM This checks cmdcmdlineline for "/c". If it exists, that means this .bat was
+REM double clicked. The "%%~x" adds quotes on the outside "" and the tilde means
+REM to remove quotes leaving only one pair of quotes and not two.
+REM This only pauses if double clicked.
+for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
+if defined DOUBLECLICKED pause
--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-python -m unittest discover -v
 
+if [ $# -eq 0 ]; then
+    python -m unittest discover -v
+elif [ $1 = "unit" ]; then
+    python -m unittest discover -v -p test_unit*.py
+elif [ $1 = "performance" ]; then
+    python -m unittest discover -v -p test_performance*.py
+else
+    echo Incorrect parameter \"$1\". Usage runtest.sh \[unit\|performance\]
+fi
