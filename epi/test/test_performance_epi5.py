37f7d67ac882275ca140a0d39231f63c72942d38
Kevin Lau <kelau1993@gmail.com>
2015-10-08 09:45:33 +0100

    Changed naming TEST_SET to SET

--- epi/test/test_performance_epi5.py
+++ epi/test/test_performance_epi5.py
@@ -9,26 +9,26 @@ class P5_Powerset_Test(unittest.TestCase):
     def setUp(self):
         self.cls = P5_Powerset
 
-        self.TEST_SET_LENGTH = 22
+        self.SET_LENGTH = 22
 
-        self.TEST_SET = set(range(self.TEST_SET_LENGTH))
+        self.SET = set(range(self.SET_LENGTH))
 
     def test_bit_array_map(self):
         bit_array_map = self.cls.bit_array_map
         wrapped = timeitextra.wrapper(bit_array_map,
-                                      self.TEST_SET)
+                                      self.SET)
         print("\n{}".format(timeit.timeit(wrapped, number=1)))
 
     def test_recursive_default(self):
         recursive_default = self.cls.recursive_default
         wrapped = timeitextra.wrapper(recursive_default,
-                                      self.TEST_SET)
+                                      self.SET)
         print("\n{}".format(timeit.timeit(wrapped, number=1)))
 
     def test_recursive_choice(self):
         recursive_choice = self.cls.recursive_choice
         wrapped = timeitextra.wrapper(recursive_choice,
-                                      self.TEST_SET)
+                                      self.SET)
         print("\n{}".format(timeit.timeit(wrapped, number=1)))
 
     def tearDown(self):
@@ -39,29 +39,29 @@ class P5_1_Subsets_Test(unittest.TestCase):
     def setUp(self):
         self.cls = P5_1_Subsets
 
-        self.TEST_SET_LENGTH = 22
+        self.SET_LENGTH = 22
 
-        self.TEST_SET = set(range(self.TEST_SET_LENGTH))
+        self.SET = set(range(self.SET_LENGTH))
 
     def test_bit_array_map(self):
         bit_array_map = self.cls.bit_array_map
         wrapped = timeitextra.wrapper(bit_array_map,
-                                      self.TEST_SET,
-                                      self.TEST_SET_LENGTH // 2)
+                                      self.SET,
+                                      self.SET_LENGTH // 2)
         print("\n{}".format(timeit.timeit(wrapped, number=1)))
 
     def test_recursive_default(self):
         recursive_default = self.cls.recursive_default
         wrapped = timeitextra.wrapper(recursive_default,
-                                      self.TEST_SET,
-                                      self.TEST_SET_LENGTH // 2)
+                                      self.SET,
+                                      self.SET_LENGTH // 2)
         print("\n{}".format(timeit.timeit(wrapped, number=1)))
 
     def test_recursive_choice(self):
         recursive_choice = self.cls.recursive_choice
         wrapped = timeitextra.wrapper(recursive_choice,
-                                      self.TEST_SET,
-                                      self.TEST_SET_LENGTH // 2)
+                                      self.SET,
+                                      self.SET_LENGTH // 2)
         print("\n{}".format(timeit.timeit(wrapped, number=1)))
 
     def tearDown(self):
