a84e27fadb3777996df02af301c5e79d6b12de8e
Kevin Lau <kelau1993@gmail.com>
2015-12-14 06:56:26 -0800

    Added comments to three_way_partition explaining how it works.

--- epi/epi6.py
+++ epi/epi6.py
@@ -11,6 +11,111 @@ class P1_ThreeWayPartitioning:
         """
         Partition L into the order
         [L[x] < L[i], L[x] == L[i], L[x] > L[i]]
+
+        This works by keeping L split into 4 partitions - less than,
+        equal, unknown, and greater than (in that order from low
+        to high index).
+        First, it puts the pivot at the 0th index. Then it starts
+        looking at unknowns from the 1st index.
+        Example:
+        [7, X, X, X, X, X, X, X, X, X, X]
+         ^  ^                          ^
+         |  |                          |greater_than_end == unknown_last
+         |  |
+         |  |unknown_start == equals_end
+         |
+         |less_than_end == equals_start
+        In the diagram, start means the index of the first element. last
+        means the index of the last element. end means last + 1.
+        The 7 is the pivot that has been placed at index 0.
+        Partitions:
+        less than - [0, less_than_end)
+        equal - [equals_start, equals_end)
+        unknown - [unknown_start, unknown_last]
+        greater than - (greater_than_end, len(L) - 1]
+        The greater than partition grows backwards.
+
+        At some point, L will look like the following.
+        Example:
+        [0, 1, 2, 7, 7, 3, X, X, X, 9, 10]
+                  ^     ^        ^
+                  |     |        |greater_than_end == unknown_last
+                  |     |
+                  |     |unknown_start == equals_end
+                  |
+                  |less_than_end == equals_start
+
+        The element at unknown_start is revealed (not and X) because this
+        example is in the middle of an iteration of the while loop where
+        3 is about to be swapped.
+
+        The first unknown, 3, is less than the pivot, 7, so it gets swapped
+        with the element at less_than_end. In effect, this adds to the less
+        than partition and rotates the equals partition.
+        Example:
+        [0, 1, 2, 3, 7, 7, X, X, X, 9, 10]
+                     ^     ^     ^
+                     |     |     |greater_than_end == unknown_last
+                     |     |
+                     |     |unknown_start == equals_end
+                     |
+                     |less_than_end == equals_start
+
+        The tracked indices are updated:
+        less_than_end += 1
+        unknown_start += 1
+        unknown_start now points to an X because the iteration is done
+        so the element is unknown again.
+
+        If the next unknown is equal to the pivot it would look like the
+        following.
+        Example:
+        [0, 1, 2, 3, 7, 7, 7, X, X, 9, 10]
+                     ^     ^     ^
+                     |     |     |greater_than_end == unknown_last
+                     |     |
+                     |     |unknown_start == equals_end
+                     |
+                     |less_than_end == equals_start
+
+        In this case, we just increment unkown_start because everything
+        is already in the right place.
+        Example:
+        [0, 1, 2, 3, 7, 7, 7, X, X, 9, 10]
+                     ^        ^  ^
+                     |        |  |greater_than_end == unknown_last
+                     |        |
+                     |        |unknown_start == equals_end
+                     |
+                     |less_than_end == equals_start
+
+        If the next unknown is greater than the pivot, it would look
+        like the following.
+        Example:
+        [0, 1, 2, 3, 7, 7, 7, 20, X, 9, 10]
+                     ^        ^   ^
+                     |        |   |greater_than_end == unknown_last
+                     |        |
+                     |        |unknown_start == equals_end
+                     |
+                     |less_than_end == equals_start
+
+        The 20 gets swapped with the element at greater_than_end.
+        Example:
+        [0, 1, 2, 3, 7, 7, 7, X, 20, 9, 10]
+                     ^        ^
+                     |        |greater_than_end == unknown_last
+                     |        |
+                     |        |unknown_start == equals_end
+                     |
+                     |less_than_end == equals_start
+
+        unknown_start stays the same because we swapped an unknown
+        in its place.
+        greater_than_end gets decremented because its partition
+        was increased.
+        In effect, this adds to the greater than partition and rotates
+        the unknown partition.
         """
 
         if (len(L) == 0):
