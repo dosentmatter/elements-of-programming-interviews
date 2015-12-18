0484b97edeed4b7acf1c8b2d628049a269ace7de
Kevin Lau <kelau1993@gmail.com>
2015-12-18 14:18:56 -0800

    Changed variable name from key to value.

--- epi/utils/listextra.py
+++ epi/utils/listextra.py
@@ -18,11 +18,11 @@ def counting_sort(L):
     value.
 
     Next, the starting indices of each value will be computed in the
-    key_starts list.
+    value_starts list.
 
     Finally, the list is sorted into a new list by putting each key in their
-    starting indices. The starting index is incremented each time a key
-    is put in place.
+    starting indices by their value. The starting index is incremented each
+    time a key is put in place.
 
     Uses O(n + k) space and O(n + k) time where k is the number of values
     for keys.
@@ -37,17 +37,17 @@ def counting_sort(L):
 
         counts[value] += 1
 
-    key_start_accumulator = 0
-    key_starts = [None] * len(counts)
+    value_start_accumulator = 0
+    value_starts = [None] * len(counts)
     for i, count in enumerate(counts):
-        key_starts[i] = key_start_accumulator
-        key_start_accumulator += count
+        value_starts[i] = value_start_accumulator
+        value_start_accumulator += count
 
     sorted_L = [None] * len(L)
     for key in L:
         value = key.value
 
-        sorted_L[key_starts[value]] = key
-        key_starts[value] += 1
+        sorted_L[value_starts[value]] = key
+        value_starts[value] += 1
 
     return sorted_L
