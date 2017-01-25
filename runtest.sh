c32da9df40db8783361d3f7e23163dca6bc56d62
Kevin Lau <kelau1993@gmail.com>
2017-01-25 14:03:26 -0800

    Change indent to 2 spaces

--- runtest.sh
+++ runtest.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-    python -m unittest discover -v
+  python -m unittest discover -v
 elif [ $1 = "unit" ]; then
-    python -m unittest discover -v -p test_unit*$2*.py
+  python -m unittest discover -v -p test_unit*$2*.py
 elif [ $1 = "performance" ]; then
-    python -m unittest discover -v -p test_performance*$2*.py
+  python -m unittest discover -v -p test_performance*$2*.py
 else
-    echo Incorrect parameter \"$1\". Usage runtest.sh [unit\|performance] [\<pattern\>]
+  echo Incorrect parameter \"$1\". Usage runtest.sh [unit\|performance] [\<pattern\>]
 fi
